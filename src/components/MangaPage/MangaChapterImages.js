3de35d1f983c5d32144754657d9e128d3298b5ac
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-07-04 17:29:42 +0200

    Added abort controller to chapter images

--- src/components/MangaPage/MangaChapterImages.js
+++ src/components/MangaPage/MangaChapterImages.js
@@ -5,22 +5,34 @@ import axios from "axios";
 const MangaChapterImages = ({ id }) => {
   const [data, setData] = useState(null);
 
+  // using the abort controller if the chapters are being changed through fast with the arrow key
+  // will abort downloading
+  const abortCont = new AbortController();
   async function handleChapterImages(url) {
-    const data = await axios.get(`${img}${url}?forcePort443=false`);
-    setData(data.data);
+    const data = await axios
+      .get(`${img}${url}?forcePort443=false`, {
+        signal: abortCont.signal,
+      })
+      .catch((err) => {
+        if (err.message === "CanceledError") {
+          return;
+        }
+      });
+    setData(data);
   }
 
   useEffect(() => {
     id && handleChapterImages(id);
+    return () => abortCont.abort();
   }, [id]);
 
   return (
     <div className="chapter">
       {data &&
-        data.chapter.dataSaver.map((img) => (
+        data.data.chapter.dataSaver.map((img) => (
           <img
             key={img}
-            src={`https://uploads.mangadex.org/data-saver/${data.chapter.hash}/${img}`}
+            src={`https://uploads.mangadex.org/data-saver/${data.data.chapter.hash}/${img}`}
             alt="manga-page"
           />
         ))}
